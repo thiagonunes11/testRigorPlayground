2854e9c51ebdccae39a9f6145758e3eb0e142db6
Thiago Nunes <thiago_nunes11@hotmail.com>
2024-04-05 14:22:28 -0300

    adicionando um suffle automático na página de drag item

--- script.js
+++ script.js
@@ -38,3 +38,11 @@ function getDragAfterElement(container, y) {
     }
   }, { offset: Number.NEGATIVE_INFINITY }).element;
 }
+
+function shuffleList(container) {
+  for (let i = container.children.length; i >= 0; i--) {
+    container.appendChild(container.children[Math.random() * i | 0]);
+  }
+}
+
+shuffleList(taskList);
