71cfcc61d5e1c821ebf8ff40cdf0c22f34e12a77
Thiago Nunes <thiago_nunes11@hotmail.com>
2024-04-16 13:22:28 -0300

    adicionando um código dinamico para a pagina de verificação de código

--- scripts/verifyCode.js
+++ scripts/verifyCode.js
@@ -1,14 +1,24 @@
-document.getElementById("verificationForm").addEventListener("submit", function(event) {
-    event.preventDefault(); 
-
-    
-    var code = document.getElementById("code").value;
-
-    
-    if (code === "1234") {
-        document.getElementById("result").innerHTML = "Valid Code";
-    } else {
-        document.getElementById("result").innerHTML = "Invalid Code. Try again";
+document.addEventListener("DOMContentLoaded", function() {
+    // Função para gerar um código aleatório de 4 dígitos
+    function generateRandomCode() {
+        return Math.floor(1000 + Math.random() * 9000);
     }
-});
 
+    // Atualiza o código exibido no HTML
+    var displayedCodeElement = document.getElementById("displayedCode");
+    var generatedCode = generateRandomCode();
+    displayedCodeElement.textContent = generatedCode;
+
+    // Adiciona o evento de envio ao formulário
+    document.getElementById("verificationForm").addEventListener("submit", function(event) {
+        event.preventDefault(); 
+
+        var code = document.getElementById("code").value;
+
+        if (code === generatedCode.toString()) {
+            document.getElementById("result").innerHTML = "Valid Code";
+        } else {
+            document.getElementById("result").innerHTML = "Invalid Code. Try again";
+        }
+    });
+});
