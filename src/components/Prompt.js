35dfe6aa55688fabb2c54fb2e9c45cb925e1f6c9
LucasCTN <lucascampostn@gmail.com>
2024-11-22 18:16:01 -0300

    Centralizes the Prompt component

--- src/components/Prompt.js
+++ src/components/Prompt.js
@@ -1,6 +1,6 @@
 function Prompt({title, instructions}){
     return(
-        <div className="border p-2 pt-4">
+        <div className="border p-2 pt-4 text-center">
             <h1>{title}</h1>
             <p><small>{instructions}</small></p>
         </div>
