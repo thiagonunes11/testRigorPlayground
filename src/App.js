b99f67c1a2f4e09be6835b8a27444c244c5a20ad
LucasCTN <lucascampostn@gmail.com>
2025-02-28 17:13:28 -0300

    Removes layout from the root file and transfer to dedicated component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import {Container, Row, Col, Navbar} from "react-bootstrap"
+import { Container, Row, Col, Navbar } from "react-bootstrap"
 
 import Footer from './components/Footer';
 //import Header from './components/Header'
@@ -30,68 +30,44 @@ import RadioButtons from './pages/radioButtons';
 function App() {
     return (
         <Router>
-            <div>
-                <Navbar fixed="top" bg="dark" variant="dark">
-                    <Container fluid>
-                        <Navbar.Brand href="/" className="mx-4">testRigor Playground 🛝</Navbar.Brand>
-                    </Container>
-                </Navbar>
-                
-                <Container fluid className="vh-100 pt-5 pb-5">
-                    <Row className="h-100 pb-4">
-                        <Col xs={3} className="bg-light sidebar h-100 pt-3 pb-2 text-center">
-                            <h4 className='pe-4'>Demos</h4>
-                            <Container fluid className="h-100 w-100 overflow-auto pe-4">
-                                <Sidebar/>
-                            </Container>
-                        </Col>
-                        <Col xs={9} className="bg-white main h-100">
-                            <div style={{ height: '100%', overflowY: 'auto' }}>
-                                <Routes>
-                                    {/*
-                                    <Route path="/audioValidation" element={<AudioValidation />} />
-                                    <Route path="/browserPrompt" element={<BrowserPrompt />} />
-                                    <Route path="/connectTheDots" element={<ConnectTheDots />} />
-                                    <Route path="/datePicker" element={<DatePicker />} />
-                                    <Route path="/deleteElements" element={<DeleteElements />} />
-                                    <Route path="/dynamicLoginText" element={<DynamicLoginText />} />
-                                    <Route path="/longClick" element={<LongClick />} />
-                                    <Route path="/modalPopup" element={<ModalPopup />} />
-                                    <Route path="/nestedIframes" element={<NestedIframes />} />
-                                    <Route path="/regex" element={<Regex />} />
-                                    <Route path="/rightClick" element={<RightClick />} />
-                                    <Route path="/scrollDown" element={<ScrollDown />} />
-                                    <Route path="/shadowDom" element={<ShadowDom />} />
-                                    <Route path="/similarPages" element={<SimilarPages />} />
-                                    <Route path="/svgElements" element={<SvgElements />} />
-                                    */}
-                                    <Route path="/dropdowns" element={<Dropdowns />} />
-                                    <Route path="/tableRelativePosition" element={<TableRelativePosition />} />
-                                    <Route path="/dynamicTable" element={<DynamicTable />} />
-                                    <Route path="/waitMessage" element={<WaitForMessage />} />
-                                    <Route path="/shoppingCart" element={<ShoppingCart />} />
-                                    <Route path="/verifyCode" element={<VerifyCode />} />
-                                    <Route path="/fileDownload" element={<FileDownload />} />
-                                    <Route path="/counter" element={<Counter />} />
-                                    <Route path="/dragItem" element={<DragItem />} />
-                                    <Route path="/mouseHover" element={<MouseHover />} />
-                                    <Route path="/openNewTab" element={<OpenNewTab />} />
-                                    <Route path="/ocrCheck" element={<OcrCheck />} />
-                                    <Route path="/newTab" element={<NewTab />} />
-                                    <Route path="/qrCode" element={<QRCodePage />} />
-                                    <Route path="/buttonClick" element={<ButtonClick />} />
-                                    <Route path="/fileUpload" element={<FileUpload />} />
-                                    <Route path="/video" element={<VideoPlayer />} />
-                                    <Route path="/checkbox" element={<CheckboxPage />} />
-                                    <Route path="/radioButtons" element={<RadioButtons />} />
-                                </Routes>
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
-
-                <Footer/>
-            </div>
+            <Routes>
+                {/*
+                <Route path="/audioValidation" element={<AudioValidation />} />
+                <Route path="/browserPrompt" element={<BrowserPrompt />} />
+                <Route path="/connectTheDots" element={<ConnectTheDots />} />
+                <Route path="/datePicker" element={<DatePicker />} />
+                <Route path="/deleteElements" element={<DeleteElements />} />
+                <Route path="/dynamicLoginText" element={<DynamicLoginText />} />
+                <Route path="/longClick" element={<LongClick />} />
+                <Route path="/modalPopup" element={<ModalPopup />} />
+                <Route path="/nestedIframes" element={<NestedIframes />} />
+                <Route path="/regex" element={<Regex />} />
+                <Route path="/rightClick" element={<RightClick />} />
+                <Route path="/scrollDown" element={<ScrollDown />} />
+                <Route path="/shadowDom" element={<ShadowDom />} />
+                <Route path="/similarPages" element={<SimilarPages />} />
+                <Route path="/svgElements" element={<SvgElements />} />
+                */}
+                <Route path="/dropdowns" element={<Dropdowns />} />
+                <Route path="/tableRelativePosition" element={<TableRelativePosition />} />
+                <Route path="/dynamicTable" element={<DynamicTable />} />
+                <Route path="/waitMessage" element={<WaitForMessage />} />
+                <Route path="/shoppingCart" element={<ShoppingCart />} />
+                <Route path="/verifyCode" element={<VerifyCode />} />
+                <Route path="/fileDownload" element={<FileDownload />} />
+                <Route path="/counter" element={<Counter />} />
+                <Route path="/dragItem" element={<DragItem />} />
+                <Route path="/mouseHover" element={<MouseHover />} />
+                <Route path="/openNewTab" element={<OpenNewTab />} />
+                <Route path="/ocrCheck" element={<OcrCheck />} />
+                <Route path="/newTab" element={<NewTab />} />
+                <Route path="/qrCode" element={<QRCodePage />} />
+                <Route path="/buttonClick" element={<ButtonClick />} />
+                <Route path="/fileUpload" element={<FileUpload />} />
+                <Route path="/video" element={<VideoPlayer />} />
+                <Route path="/checkbox" element={<CheckboxPage />} />
+                <Route path="/radioButtons" element={<RadioButtons />} />
+            </Routes>
         </Router>
     );
 }
--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,8 +1,30 @@
-function Demo({children}){
-    return(
-        <main className="container mt-5 text-center">
-            {children}
-        </main>
+import {Container, Row, Col, Navbar} from "react-bootstrap"
+
+import Footer from './Footer';
+//import Header from './Header'
+import Sidebar from './Sidebar';
+
+function Demo({ children }) {
+    return (
+        <div>
+            <Navbar fixed="top" bg="dark" variant="dark">
+                <Container fluid>
+                    <Navbar.Brand href="/" className="mx-4">testRigor Playground 🛝</Navbar.Brand>
+                </Container>
+            </Navbar>
+
+            <Container fluid className="vh-100 pt-5 pb-5">
+                <Row className="h-100 pb-4">
+                    <Col className="bg-white main">
+                        <main className="container mt-5 text-center">
+                            {children}
+                        </main>
+                    </Col>
+                </Row>
+            </Container>
+
+            <Footer />
+        </div >
     )
 }
 
