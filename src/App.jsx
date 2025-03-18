ed4d0fdcaf9be3fd8df864a4fb1270ad27902093
Mariana Soffer <marianasoffer@gmail.com>
2025-03-18 12:16:31 -0300

    Added audioValidation Page

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ import BrowserPrompt from './pages/browserPrompt';
 import BrowserPromptOnLoad from './pages/browserPromptOnLoad';
 import LongClick from './pages/longClick'; 
 import DeleteElements from "./pages/deleteElements";
-
+import AudioValidation from './pages/audioValidation';
 
 function App() {
     return (
@@ -58,6 +58,7 @@ function App() {
                 <Route path="/similarPages" element={<SimilarPages />} />
                 <Route path="/svgElements" element={<SvgElements />} />
                 */}
+                <Route path="/audioValidation" element={<AudioValidation />} />
                 <Route path="/dropdowns" element={<Dropdowns />} />
                 <Route path="/BrowserPrompt" element={<BrowserPrompt />} />
                 <Route path="/BrowserPromptOnLoad" element={<BrowserPromptOnLoad />} />
