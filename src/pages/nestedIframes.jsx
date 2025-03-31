1a8605f8c8a9e0722d9c653a3c9ec7cf5e115504
Mariana Soffer <marianasoffer@gmail.com>
2025-03-31 13:01:50 -0300

    nestedIframe Responsive

--- src/pages/nestedIframes.jsx
+++ src/pages/nestedIframes.jsx
@@ -1,35 +1,40 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import FirstIframe from './nestedIframes/firstIframe'; // Import the FirstIframe component
 import Demo from '../components/Demo.jsx';
 
 const NestedIframes = () => {
   return (
     <Demo>
-    
-      <main className="container mt-5">
-        <div className="row justify-content-center text-center">
-          <div className="col-6 border p-2">
+      <Container className="mt-5">
+        <Row className="justify-content-center text-center">
+          <Col xs={12} md={10} lg={6} className="border p-2">
             <h1 className="fs-2 fw-bold mt-3 mb-4">Nested Iframes</h1>
             <p>
               <small>
                 There are two iframes, one inside another. Both of them have a unique word that is a link. This link changes the iframe url, changing also the text.
               </small>
             </p>
-          </div>
-        </div>
+          </Col>
+        </Row>
 
-        <div className="row mt-5">
-          <div className="col">
+        <Row className="mt-5">
+          <Col xs={12}>
             <h3>Out of iframe</h3>
             <p>This first test is present on the current page, outside of any iframe.</p>
 
-            {/* Replace iframe with the FirstIframe component */}
-            <iframe src="http://localhost:5173/nestedIframes/firstIframe" width="1000px" height="800px">
-            </iframe>
-           
-          </div>
-        </div>
-      </main>
+            {/* Responsive iframe container */}
+            <div className="ratio ratio-16x9">
+              <iframe 
+                src="http://localhost:5173/nestedIframes/firstIframe" 
+                allowFullScreen
+                title="Nested Iframes Demo"
+              >
+              </iframe>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </Demo>
   );
 };
