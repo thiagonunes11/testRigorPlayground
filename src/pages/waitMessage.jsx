79696ae7003e4355dfc03abb176ad1c4e083dfd6
LucasCTN <lucascampostn@gmail.com>
2025-04-24 16:52:36 -0300

    Fixes the demo for WaitMessage

--- src/pages/waitMessage.jsx
+++ src/pages/waitMessage.jsx
@@ -1,50 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import Layout from '../components/Layout';
+import { Collapse, Alert, Row, Col } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
 
-const WaitMessage = () => {
-    useEffect(() => {
-        let counter = 5;
-
-        function callTimeout() {
-            document.getElementById("counter").innerHTML = counter;
-
-            if (counter > 0) {
-                counter--;
-                setTimeout(callTimeout, 1000);
-            } else {
-                document.getElementById("secretMessage").hidden = false;
-            }
-        }
-
-        callTimeout();
-    }, []);
-
-    return (
-        <main className="container mt-5">
-            <div className="row justify-content-center text-center">
-                <div className="col-6 border p-2 pt-4">
-                    <h1 className="fs-2 fw-bold">Wait for the Message</h1>
-                    <p><small>In five seconds, the message will appear.</small></p>
-                </div>
-            </div>
-
-            <div className="row mt-5 justify-content-center text-center">
-                <h3>The secret message will appear in <span id="counter">3</span>...</h3>
-
-                <div className="col-4 mt-3">
-                    <div id="secretMessage" className="alert alert-success pb-0" role="alert" hidden>
-                        <p>The secret message is: banana</p>
-                    </div>
-                </div>
-            </div>
-        </main>
-    );
-};
-
-
-import Prompt from '../components/Prompt.jsx'
-import Layout from '../components/Layout';import { Collapse, Alert, Row, Col } from 'react-bootstrap';
-
-function WaitForMessage() {
+function WaitMessage() {
     const [secondsLeft, setSecondsLeft] = useState(5);
 
     useEffect(() => {
