ecd816055a6afb5cfcfffb30768e4ba70a8425ec
Mariana Soffer <marianasoffer@gmail.com>
2025-05-08 11:29:04 -0300

    fixed nestedIframes

--- src/pages/nestedIframes.jsx
+++ src/pages/nestedIframes.jsx
@@ -3,23 +3,16 @@ import { Container, Row, Col } from 'react-bootstrap';
 import FirstIframe from './nestedIframes/firstIframe'; // Import the FirstIframe component
 import Layout from '../components/Layout';
 
+
+
 const NestedIframes = () => {
   return (
     <Layout
       title={"Nested Iframes"}
       description={"There are two iframes, one inside another. Both of them have a unique word that is a link. This link changes the iframe url, changing also the text."}
     >
-      <Container className="mt-5">
-        <Row className="justify-content-center text-center">
-          <Col xs={12} md={10} lg={6} className="border p-2">
-            <h1 className="fs-2 fw-bold mt-3 mb-4">Nested Iframes</h1>
-            <p>
-              <small>
-                There are two iframes, one inside another. Both of them have a unique word that is a link. This link changes the iframe url, changing also the text.
-              </small>
-            </p>
-          </Col>
-        </Row>
+      <Container className="mt-5 mb-3">
+        
 
         <Row className="mt-5">
           <Col xs={12}>
