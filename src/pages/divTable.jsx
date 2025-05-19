599c8fb2b059d136a3d85f09d4de15dbbe34c149
Thiago Nunes <thiago_nunes11@hotmail.com>
2025-05-19 15:29:15 -0300

    hotfix divTable

--- src/pages/divTable.jsx
+++ src/pages/divTable.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import { Row, Col, Table } from 'react-bootstrap';
 import '../styles/homePage.css';
 
 const DivTable = () => {
@@ -9,36 +8,36 @@ const DivTable = () => {
       title="HTML Div Table"
       description="Verify contents of table below."
     >
-      <Row className="mt-5 demo-content">
-        <Col>
-          <Table className="text-start dynamic-table" striped bordered hover>
-            <thead>
-              <tr>
-                <th>head1</th>
-                <th>head2</th>
-                <th>head3</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>ABC</td>
-                <td>DEF</td>
-                <td>GHI</td>
-              </tr>
-              <tr>
-                <td>QWE</td>
-                <td>RTY</td>
-                <td>UIO</td>
-              </tr>
-              <tr>
-                <td>MNB</td>
-                <td>VCX</td>
-                <td>ASD</td>
-              </tr>
-            </tbody>
-          </Table>
-        </Col>
-      </Row>
+      <div className="row justify-content-center">
+        <div className="col-12 col-md-8">
+          <div className="divTable blueTable">
+            <div className="divTableHeading">
+              <div className="divTableRow">
+                <div className="divTableHead">head1</div>
+                <div className="divTableHead">head2</div>
+                <div className="divTableHead">head3</div>
+              </div>
+            </div>
+            <div className="divTableBody">
+              <div className="divTableRow">
+                <div className="divTableCell">ABC</div>
+                <div className="divTableCell">DEF</div>
+                <div className="divTableCell">GHI</div>
+              </div>
+              <div className="divTableRow">
+                <div className="divTableCell">QWE</div>
+                <div className="divTableCell">RTY</div>
+                <div className="divTableCell">UIO</div>
+              </div>
+              <div className="divTableRow">
+                <div className="divTableCell">MNB</div>
+                <div className="divTableCell">VCX</div>
+                <div className="divTableCell">ASD</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
