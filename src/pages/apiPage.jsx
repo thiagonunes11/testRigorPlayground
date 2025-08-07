d36af189f8210654fb5081ff7c27bd7bcd14a454
Thiago Nunes <thiago_nunes11@hotmail.com>
2025-08-07 14:38:26 -0300

    netlify deploy fix attempt 1

--- src/pages/apiPage.jsx
+++ src/pages/apiPage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import Layout from '../components/Layout';
 import '../styles/homePage.css';
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3001";
+const API_URL = import.meta.env.VITE_API_URL || "https://api-testrigor.onrender.com";
 
 const ApiPage = () => {
   // GET /code
