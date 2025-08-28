f0cabff973b53afe90e1546d05b2dfc6bf217f04
Thiago Nunes <thiago_nunes11@hotmail.com>
2025-08-28 10:52:24 -0300

    adding more capabilities to os browser page

--- src/pages/osBrowser.jsx
+++ src/pages/osBrowser.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useEffect, useState } from 'react';
 import Layout from '../components/Layout';
 import '../styles/homePage.css';
 
@@ -9,7 +9,8 @@ const getBrowserInfo = () => {
   let version = 'Unknown';
 
   if (/Edg\//.test(ua)) name = 'Microsoft Edge';
-  else if (/OPR\//.test(ua) || /Opera/.test(ua)) name = 'Opera';
+  else if (/Vivaldi/i.test(ua)) name = 'Vivaldi';
+  else if (/OPR\//.test(ua) || /Opera/i.test(ua)) name = 'Opera';
   else if (/Chrome\//.test(ua)) name = 'Chrome';
   else if (/Safari\//.test(ua) && /Version\//.test(ua)) name = 'Safari';
   else if (/Firefox\//.test(ua)) name = 'Firefox';
@@ -18,16 +19,24 @@ const getBrowserInfo = () => {
   const versionMatch = ua.match(/(Edg|OPR|Chrome|Version|Firefox|MSIE|rv):?\/?([\d\.]+)/);
   if (versionMatch && versionMatch[2]) version = versionMatch[2];
 
-  // Optionally refine using userAgentData.brands when available
   const brand = navigator.userAgentData?.brands?.[0]?.brand;
-  if (brand) name = name === 'Unknown' ? brand : name;
+  if (brand && name === 'Unknown') name = brand;
 
   return { name, version };
 };
 
+const parseOsVersionFromUA = (ua) => {
+  // Android 13; Pixel 6 -> Android 13
+  const android = ua.match(/Android\s([\d._]+)/);
+  if (android) return android[1].replace(/_/g, '.');
+  // iOS: like CPU iPhone OS 16_5 or CPU OS 15_7
+  const ios = ua.match(/OS\s([\d_]+)\slike\sMac\sOS\sX|iPhone\sOS\s([\d_]+)/);
+  if (ios) return (ios[1] || ios[2]).replace(/_/g, '.');
+  return undefined;
+};
+
 const getOSInfo = () => {
   const ua = navigator.userAgent;
-  // Prefer userAgentData.platform when available; otherwise, infer from UA
   const platform = navigator.userAgentData?.platform ||
     (/(Windows|Macintosh|Linux|Android|iPhone|iPad|iPod)/.exec(ua)?.[0]) || 'Unknown';
 
@@ -42,12 +51,29 @@ const getOSInfo = () => {
   else if (/(iPhone|iPad|iPod)/.test(ua)) name = 'iOS';
   else if (/Linux/.test(ua)) name = 'Linux';
 
-  return { name, platform };
+  const version = parseOsVersionFromUA(ua) || navigator.userAgentData?.platformVersion;
+
+  return { name, platform, version };
 };
 
 const OsBrowser = () => {
   const browser = useMemo(getBrowserInfo, []);
   const os = useMemo(getOSInfo, []);
+  const [deviceModel, setDeviceModel] = useState('Unavailable');
+  const [platformVersion, setPlatformVersion] = useState(os.version || 'Unknown');
+
+  useEffect(() => {
+    // Try high-entropy UA-CH for model and platformVersion on supporting browsers (Chromium on Android)
+    const uad = navigator.userAgentData;
+    if (uad?.getHighEntropyValues) {
+      uad.getHighEntropyValues(['platform', 'platformVersion', 'model'])
+        .then((hints) => {
+          if (hints.model) setDeviceModel(hints.model);
+          if (hints.platformVersion && !os.version) setPlatformVersion(hints.platformVersion);
+        })
+        .catch(() => {});
+    }
+  }, [os.version]);
 
   return (
     <Layout
@@ -60,6 +86,8 @@ const OsBrowser = () => {
           <div className="info-block">
             <p><b>Name:</b> {os.name}</p>
             <p><b>Platform:</b> {os.platform}</p>
+            <p><b>Version:</b> {platformVersion}</p>
+            <p><b>Device model:</b> {deviceModel}</p>
           </div>
         </div>
         <div className="modern-card">
